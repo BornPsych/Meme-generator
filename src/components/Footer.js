d64bb02a0b7a47c114a23bfb166c7ef573ac4a69
BornPsych <yogeshshahi120@gmail.com>
2023-01-03 02:05:10 +0530

    Final meme generator

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Created with ❤️ by &nbsp;
+        <a className='website-link' href='https://github.com/BornPsych'>
+          Yogesh Shahi
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
